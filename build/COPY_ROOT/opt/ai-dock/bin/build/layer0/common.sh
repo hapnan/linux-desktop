55eadc9ee074d2e581c7be9eb47cd5d0ad35db0e
hapnan <hapnanarsad@gmail.com>
2024-07-16 22:35:53 +0700

    selkies name fix

--- build/COPY_ROOT/opt/ai-dock/bin/build/layer0/common.sh
+++ build/COPY_ROOT/opt/ai-dock/bin/build/layer0/common.sh
@@ -344,7 +344,8 @@ function build_common_install_selkies() {
         SELKIES_VERSION="$(curl -fsSL "https://api.github.com/repos/selkies-project/selkies-gstreamer/releases/latest" \
             | jq -r '.tag_name' | sed 's/[^0-9\.\-]*//g')"
     fi
-    
+    sudo pipx ensurepath --global 
+
     cd /opt
     version_id=$(grep -oP 'VERSION_ID="\K[^"]+' /etc/os-release)
     curl -L "https://github.com/selkies-project/selkies-gstreamer/releases/download/v${SELKIES_VERSION}/gstreamer-selkies_gpl_v${SELKIES_VERSION}_ubuntu${version_id}_amd64.tar.gz" | tar -zxf -
@@ -352,14 +353,13 @@ function build_common_install_selkies() {
     cd /tmp
     curl -L -O "https://github.com/selkies-project/selkies-gstreamer/releases/download/v${SELKIES_VERSION}/selkies_gstreamer-${SELKIES_VERSION}-py3-none-any.whl"
     pipx install "selkies_gstreamer-${SELKIES_VERSION}-py3-none-any.whl"
-    pipx ensurepath
 
     cd /opt
     curl -L "https://github.com/selkies-project/selkies-gstreamer/releases/download/v${SELKIES_VERSION}/selkies-gstreamer-web_v${SELKIES_VERSION}.tar.gz" | tar -zxf -
     
     cd /tmp
-    curl -L -o selkies-js-interposer.deb "https://github.com/selkies-project/selkies-gstreamer/releases/download/v${SELKIES_VERSION}/selkies_js_interposer_v${SELKIES_VERSION}_ubuntu${version_id}_amd64.deb"
-    $APT_INSTALL ./selkies-js-interposer.deb
+    curl -L "https://github.com/selkies-project/selkies-gstreamer/releases/download/v${SELKIES_VERSION}/selkies_js_interposer_v${SELKIES_VERSION}_ubuntu${version_id}_amd64.deb"
+    $APT_INSTALL ./selkies-js-interposer_v1.6.1_ubuntu24.04_amd64.deb
 }
 
 function build_common_install_coturn() {
